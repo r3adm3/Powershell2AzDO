7114662ea3eb9d8b121dfdded8c908d4182119e4
Adrian Farnell <adrianjfarnell@gmail.com>
2021-01-14 10:24:07 +0000

    added get project powershell

--- getSpecificProj.ps1
+++ getSpecificProj.ps1
@@ -4,7 +4,7 @@ $OrganizationName = "techfrontier"
 $AzureDevOpsAuthenicationHeader = @{Authorization = 'Basic ' + [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(":$($AzureDevOpsPAT)")) }
 
 $UriOrga = "https://dev.azure.com/$($OrganizationName)/" 
-$uriAccount = $UriOrga + "_apis/projects?api-version=5.1"
+$uriAccount = $UriOrga + "_apis/projects/1348193c-3d50-468a-85af-11889bf26628?includeCapabilities=True&api-version=5.1"
 
 $output = Invoke-RestMethod -Uri $uriAccount -Method get -Headers $AzureDevOpsAuthenicationHeader 
 
@@ -12,6 +12,4 @@ write-host "  ** Headers   : $($AzureDevOpsAuthenicationHeader['Authorization'])
 write-host "  ** uriAccout : $($uriAccount)"
 write-host ""
 
-$output.value | ForEach-Object {
-    Write-Host $_.id "(" $_.name ")" 
-}
+$output
